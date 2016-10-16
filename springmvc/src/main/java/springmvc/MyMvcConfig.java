f82e7b0f2b52ea848679a6612a259f1a2c6e6ade
moon <li1215101@163.com>
2016-10-16 22:52:01 +0800

    拦截器

--- springmvc/src/main/java/springmvc/MyMvcConfig.java
+++ springmvc/src/main/java/springmvc/MyMvcConfig.java
@@ -1,9 +1,13 @@
 package springmvc;
 
+import interceptor.DemoInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -15,7 +19,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan("springmvc")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
@@ -24,5 +28,30 @@ public class MyMvcConfig {
         internalResourceViewResolver.setSuffix(".jsp");
         internalResourceViewResolver.setViewClass(JstlView.class);
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DemoInterceptor demoInterceptor() {
+        return new DemoInterceptor();
+    }
+
+    /**
+     * 注册拦截器
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(demoInterceptor());
+    }
+
+    /**
+     * 不需要拦截
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
     }
 }
