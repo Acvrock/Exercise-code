65e1bafb9ba699f679a48dd61f229a8fce90170b
moon <li1215101@163.com>
2016-11-12 22:44:02 +0800

    servlet 数据校验

---
+++ app02c/src/main/java/com/acvrock/validator/ProductValidator.java
@@ -0,0 +1,28 @@
+package com.acvrock.validator;
+
+import com.acvrock.form.ProductForm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductValidator {
+	
+	public List<String> validate(ProductForm productForm) {
+		List<String> errors = new ArrayList<String>();
+		String name = productForm.getName();
+		if (name == null || name.trim().isEmpty()) {
+			errors.add("Product must have a name");
+		}
+		String price = productForm.getPrice();
+		if (price == null || price.trim().isEmpty()) {
+			errors.add("Product must have a price");
+		} else {
+			try {
+				Float.parseFloat(price);
+			} catch (NumberFormatException e) {
+				errors.add("Invalid price value");
+			}
+		}
+		return errors;
+	}
+}
