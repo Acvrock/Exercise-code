dd89099ed1c60222f655dd12e8123269df1b66b7
moon <li1215101@163.com>
2016-11-13 16:18:33 +0800

    引入 spring mvc

---
+++ app03a/src/main/java/com/acvrock/controller/InputProductController.java
@@ -0,0 +1,23 @@
+package com.acvrock.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InputProductController implements Controller {
+
+    private static final Log logger = LogFactory
+            .getLog(InputProductController.class);
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        logger.info("InputProductController called");
+        return new ModelAndView("/WEB-INF/jsp/ProductForm.jsp");
+    }
+
+}
