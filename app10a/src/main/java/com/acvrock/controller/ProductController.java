3fbe88e942322157b307dae153ff1d1213f340bd
moon <li1215101@163.com>
2016-11-17 17:36:47 +0800

    国际化支持

---
+++ app10a/src/main/java/com/acvrock/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.acvrock.controller;
+
+import com.acvrock.domain.Product;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+
+public class ProductController {
+	
+	private static final Log logger = LogFactory.getLog(ProductController.class);
+	
+	@RequestMapping(value="/product_input")
+	public String inputProduct(Model model) {
+		model.addAttribute("product", new Product());
+		return "ProductForm";
+	}
+
+	@RequestMapping(value="/product_save")
+	public String saveProduct(@Valid @ModelAttribute Product product, BindingResult bindingResult,
+			Model model) {
+		
+		// save product here
+		
+	    model.addAttribute("product", product);
+		return "ProductDetails";
+	}
+	
+}
