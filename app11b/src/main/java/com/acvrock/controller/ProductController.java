75f54bd679c396cdc04e78c55dca2cd83898e748
moon <li1215101@163.com>
2016-11-19 22:59:04 +0800

    文件上传和访问控制相关知识点

---
+++ app11b/src/main/java/com/acvrock/controller/ProductController.java
@@ -0,0 +1,62 @@
+package com.acvrock.controller;
+
+import com.acvrock.domain.Product;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+
+public class ProductController {
+	
+	private static final Log logger = LogFactory.getLog(ProductController.class);
+	
+	@RequestMapping(value="/product_input")
+	public String inputProduct(Model model) {
+		model.addAttribute("product", new Product());
+		logger.info("inputProduct called 2c");
+		return "ProductForm";
+	}
+
+    @RequestMapping(value = "/product_save")
+    public String saveProduct(HttpServletRequest servletRequest,
+            @ModelAttribute Product product, BindingResult bindingResult,
+            Model model) {
+
+        List<MultipartFile> files = product.getImages();
+
+        List<String> fileNames = new ArrayList<String>();
+
+        if (null != files && files.size() > 0) {
+            for (MultipartFile multipartFile : files) {
+
+                String fileName = multipartFile.getOriginalFilename();
+                fileNames.add(fileName);
+
+                File imageFile = new File(servletRequest.getServletContext()
+                        .getRealPath("/image"), fileName);
+                try {
+                    multipartFile.transferTo(imageFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // save product here
+        model.addAttribute("product", product);
+        return "ProductDetails";
+    }
+	
+}
---
+++ app11b/src/main/java/com/acvrock/validator/ProductValidator.java
@@ -0,0 +1,24 @@
+package com.acvrock.validator;
+
+import com.acvrock.domain.Product;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+public class ProductValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> klass) {
+        return Product.class.isAssignableFrom(klass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        System.out.println("============== validating product");
+        Product product = (Product) target;
+        ValidationUtils.rejectIfEmpty(errors, "name", "productname.required");
+        Float price = product.getPrice();
+        if (price != null && price < 0) {
+            errors.rejectValue("price", "price.negative");
+        }
+    }
+}
