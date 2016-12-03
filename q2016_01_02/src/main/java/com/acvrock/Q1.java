1b69b8ad32e320dc6fcba570ea71a9fdc3e8114a
moon <li1215101@163.com>
2016-12-03 23:23:20 +0800

    写 Java 编码方向的试验

---
+++ q2016_01_02/src/main/java/com/acvrock/Q1.java
@@ -0,0 +1,77 @@
+package com.acvrock;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+/**
+ * Created by moon on 03/12/2016.
+ *
+ * @Description: Q:1  得到 String s="中国" 这个字符串的utf-8编码，gbk编码，iso-8859-1编码的字符串，看看各自有多少字节，同时解释为什么以utf-8编码得到的byte[]无法用gbk的方式“还原”为原来的字符串
+ * A:
+ */
+public class Q1 {
+
+    /**
+     * 8 位 UCS 转换格式
+     */
+    public static final String UTF_8 = "UTF-8";
+    /**
+     * 中文超大字符集
+     */
+    public static final String GBK = "GBK";
+    /**
+     * ISO 拉丁字母表 No.1，也叫作 ISO-LATIN-1
+     */
+    public static final String ISO_8859_1 = "ISO-8859-1";
+
+    /**
+     * @param str
+     * @param charSet
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static int getStringByteLength(String str, String charSet) throws UnsupportedEncodingException {
+        return str.getBytes(charSet).length;
+    }
+
+    /**
+     * @param str
+     * @param oldCharSet
+     * @param newCharSet
+     * @return
+     * @throws UnsupportedEncodingException
+     */
+    public static String changeCharSet(String str, String oldCharSet, String newCharSet) throws UnsupportedEncodingException {
+        System.out.println("默认编码 bytes " + Arrays.toString(str.getBytes()));
+        byte[] bytes = str.getBytes(oldCharSet);
+        System.out.println(oldCharSet + "编码 bytes " + Arrays.toString(bytes));
+        String s = new String(bytes, newCharSet);
+        System.out.println(newCharSet + "编码 bytes " + Arrays.toString(s.getBytes(newCharSet)));
+        return s;
+    }
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String china = "中国";  // 默认随系统，这里是 UTF-8
+        String chinaUTF8 = changeCharSet(china, UTF_8, UTF_8);
+        System.out.println("UTF-8 编码字符串:" + chinaUTF8);
+        int chinaUTF8Length = getStringByteLength(chinaUTF8, UTF_8);
+        System.out.println("字节数组长度" + chinaUTF8Length);
+        System.out.println();
+        String chinaGBK = changeCharSet(china, GBK, GBK);
+        System.out.println("GBK 编码字符串:" + chinaGBK);
+        int chinaGBKLength = getStringByteLength(chinaGBK, GBK);
+        System.out.println("字节数组长度" + chinaGBKLength);
+        System.out.println();
+        String chinaISO = changeCharSet(china, ISO_8859_1, ISO_8859_1);
+        System.out.println("ISO_8859_1 编码字符串:" + chinaISO);
+        int chinaISOLength = getStringByteLength(chinaISO, ISO_8859_1);
+        System.out.println("字节数组长度" + chinaISOLength);
+        System.out.println();
+        String chinaGBK1 = changeCharSet(china, UTF_8, GBK);
+        System.out.println("UTF-8 转 GBK 编码字符串:" + chinaGBK1);
+        int chinaGBK1Length = getStringByteLength(chinaGBK1, GBK);
+        System.out.println("字节数组长度" + chinaGBK1Length);
+
+
+    }
+}
---
+++ q2016_01_02/src/main/java/com/acvrock/Q2.java
@@ -0,0 +1,75 @@
+package com.acvrock;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+/**
+ * Created by moon on 03/12/2016.
+ *
+ * @Description: Q 2 分别用大头和小头模式将整数 a=10240写入到文件中（4个字节），并且再正确读出来，打印到屏幕上，同时截图UltraEdit里的二进制字节序列，做对比说明
+ */
+public class Q2 {
+    public static void main(String[] args) throws IOException {
+        int a = 10240;
+        byte[] bigEndianArray = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(a).array();
+        byte[] litileEndianArray = ByteBuffer.allocate(4).order(ByteOrder.LITTLE_ENDIAN).putInt(a).array();
+        System.out.println("BIG_ENDIAN："+Arrays.toString(bigEndianArray));
+        System.out.println("LITTLE_ENDIAN："+Arrays.toString(litileEndianArray));
+        write("BIG_ENDIAN", bigEndianArray);
+        write("LITTLE_ENDIAN", litileEndianArray);
+        show("BIG_ENDIAN",ByteOrder.BIG_ENDIAN);
+        show("LITTLE_ENDIAN",ByteOrder.LITTLE_ENDIAN);
+    }
+
+    /**
+     * 一行代码写文件的封装.
+     *
+     * @param paths
+     * @param content
+     * @throws IOException
+     */
+    private static void write(String paths, Iterable<? extends CharSequence> content) throws IOException {
+        Files.write(Paths.get(paths), content);
+    }
+
+    /**
+     * 一行代码写文件的封装
+     *
+     * @param paths
+     * @param content
+     * @throws IOException
+     */
+    private static void write(String paths, byte[] content) throws IOException {
+        Files.write(Paths.get(paths), content);
+    }
+
+
+    /**
+     * 一行代码输出文件
+     * <p>
+     * java 8 一行代码读取文件内容
+     * <p>
+     * 1）文件可能很大，可能会超出内存空间，使用前要做评估。
+     * 2）要输出日志，记录为什么无法读取文件或者在阅读文件时遇到的任何错误。
+     * 3）在把字节转换成字符时，应该指定字符编码。
+     * 4）要处理文件不存在的情况。
+     * 还要注意，如果读入的文件的编码是ANSI编码，那么上面的例子在读取文件内容时会报java.nio.charset.MalformedInputException: Input length = 1错误。
+     *
+     * @param filepath
+     *  @param byteOrder
+     * @throws IOException
+     */
+    private static void show(String filepath,ByteOrder byteOrder) throws IOException {
+        Files.readAllLines(Paths.get(filepath), Charset.defaultCharset()).forEach(t -> {
+
+            int x = java.nio.ByteBuffer.wrap(t.getBytes()).order(byteOrder).getInt();
+
+            System.out.println(filepath + "------>#" +x);
+        });
+    }
+}
