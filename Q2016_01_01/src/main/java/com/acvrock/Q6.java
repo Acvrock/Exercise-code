baa4f9a26d938d4309c27c79619f2051f7017d5f
moon <li1215101@163.com>
2016-12-07 21:41:02 +0800

    修改参数

--- Q2016_01_01/src/main/java/com/acvrock/Q6.java
+++ Q2016_01_01/src/main/java/com/acvrock/Q6.java
@@ -9,7 +9,7 @@ import java.util.*;
  */
 public class Q6 {
     public static void main(String[] args) {
-//        new byte[1 << 16]
+        System.out.println(1 << 13);
         Random random = new Random();
         List<int[]> arrays = new ArrayList<>();
         arrays.add(random.ints().limit(1 << 20).toArray());
@@ -32,14 +32,13 @@ public class Q6 {
         arrays.add(random.ints().limit(1 << 3).toArray());
         arrays.add(random.ints().limit(1 << 2).toArray());
         arrays.add(random.ints().limit(1 << 1).toArray());
-
         for (int i = 0; i < arrays.size(); i++) {
             long start1 = new Date().getTime();
             for (int j = 0; j <(1 << (i+1)); j++) {
                 Arrays.parallelSort(arrays.get(i));
             }
             long end1 = new Date().getTime();
-            System.out.println(end1 - start1+"::: "+arrays.get(i).length);
+            System.out.println(end1 - start1+"ms 长度 "+arrays.get(i).length);
         }
     }
 }
