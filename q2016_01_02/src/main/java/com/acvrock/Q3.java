3ad0194ba926002059381876d6f20a2cf1d21fa8
moon <li1215101@163.com>
2016-12-13 23:26:53 +0800

    copy Q3

---
+++ q2016_01_02/src/main/java/com/acvrock/Q3.java
@@ -0,0 +1,92 @@
+package com.acvrock;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * Created by moon on 12/12/2016.
+ *
+ * @Description:
+ */
+public class Q3  {
+    static final int W = 10000;
+    static final int BASESALARYMAX = 100 * W;
+    static final int BASESALARYMIN = 5 * W;
+    static final int BASESALARYRYBOUND = BASESALARYMAX - BASESALARYMIN + 1;
+    static final int BOUNDMAX = 10 * W;
+    static final int BOUNDBOUND = BOUNDMAX + 1;
+    static final String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+   static Salary[] salaries = new Salary[10000];
+
+   static {
+       Arrays.setAll(salaries,);
+   }
+
+
+    public static void main(String[] args) {
+        Salary[] salaries = new Salary[10000];
+        Random random = new Random();
+        List<Salary> collect = Arrays.stream(salaries).map(salary -> {
+                    salary = new Salary();
+                    salary.setBaseSalary(random.nextInt(BASESALARYRYBOUND) + BASESALARYMIN);
+                    salary.setBonus(random.nextInt(BOUNDBOUND));
+                    StringBuffer buf = new StringBuffer();
+                    for (int i = 0; i < 5; i++) {
+                        int num = random.nextInt(62);
+                        buf.append(str.charAt(num));
+                    }
+                    salary.setName(buf.toString());
+                    return salary;
+                }
+        ).sorted().limit(10).collect(Collectors.toList());
+        for (Salary salary : collect) {
+            System.out.println(salary);
+        }
+    }
+
+    static class Salary implements Comparable {
+        private String name;
+        private int baseSalary;
+        private int bonus;
+
+        public int compareTo(Object o) {
+            Salary salary = (Salary) o;
+            return Integer.compare(salary.getBaseSalary() * 13 + salary.getBonus(), baseSalary * 13 + bonus);
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setBaseSalary(int baseSalary) {
+            this.baseSalary = baseSalary;
+        }
+
+        public void setBonus(int bonus) {
+            this.bonus = bonus;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getBaseSalary() {
+            return baseSalary;
+        }
+
+        public int getBonus() {
+            return bonus;
+        }
+
+        @Override
+        public String toString() {
+            return "Salary{" +
+                    "name='" + name + '\'' +
+                    ", baseSalary=" + baseSalary +
+                    ", bonus=" + bonus +
+                    '}';
+        }
+    }
+}
