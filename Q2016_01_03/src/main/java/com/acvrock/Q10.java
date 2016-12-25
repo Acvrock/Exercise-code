dacc4d71f6e1efc023e0bdc3be600fb73a28f519
moon <li1215101@163.com>
2016-12-25 14:49:48 +0800

    集合作业完成一半

---
+++ Q2016_01_03/src/main/java/com/acvrock/Q10.java
@@ -0,0 +1,40 @@
+package com.acvrock;
+
+import java.util.IdentityHashMap;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * Created by moon on 24/12/2016.
+ *
+ * @Description:
+ */
+public class Q10 {
+    public static void main(String[] args) {
+        IdentityHashMap<Object, Object> map = new IdentityHashMap();
+        Integer a=5;
+        Integer b=5;
+        map.put(a,"100");
+        map.put(b,"100");
+        System.out.println(map.size());
+        map.clear();
+        a = Integer.MAX_VALUE - 1;
+        b = Integer.MAX_VALUE - 1;
+        map.put(a,"100");
+        map.put(b,"100");
+        System.out.println(map.size());
+        map.clear();
+        a = 127;
+        b = 127;
+        map.put(a,"100");
+        map.put(b,"100");
+        System.out.println(map.size());
+        map.clear();
+        a = 128;
+        b = 128;
+        map.put(a,"100");
+        map.put(b,"100");
+        System.out.println(map.size());
+        ArrayBlockingQueue arrayBlockingQueue = new ArrayBlockingQueue();
+
+    }
+}
