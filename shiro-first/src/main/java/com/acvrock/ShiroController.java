61a2511995f2ae54c952919c4730ca6127bf7b0b
moon <li1215101@163.com>
2017-01-15 22:38:17 +0800

    shiro 相关

---
+++ shiro-first/src/main/java/com/acvrock/ShiroController.java
@@ -0,0 +1,63 @@
+package com.acvrock;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by moon on 15/01/2017.
+ *
+ * @Description:
+ */
+@Controller
+public class ShiroController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping({ "/", "/index" })
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @RequestMapping("/forbidden")
+    public String forbidden() {
+        return "forbidden";
+    }
+
+    @RequestMapping("/logout")
+    public String logout() {
+        SecurityUtils.getSubject().logout();
+        return "redirect:index";
+    }
+
+    @RequestMapping("/login")
+    public String login(String username, String password) {
+        Subject currentUser = SecurityUtils.getSubject();
+        if (StringUtils.hasText(username) && StringUtils.hasText(password)) {
+            try {
+                currentUser.login(new UsernamePasswordToken(username, password));
+            } catch (Exception e) {
+                logger.error(e.getLocalizedMessage(), e);
+                return "login";
+            }
+            return "redirect:index";
+        } else {
+            return "login";
+        }
+    }
+}
